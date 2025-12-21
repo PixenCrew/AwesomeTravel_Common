fc2be1549a7fc8500a5a91080f27de962b57cf36
hhhhea <haechang977@gmail.com>
2025-12-21 17:30:51 +0900

    KKJJ-56 <refactor> (<Banner>) : 배너 공용사용을 위한 필드 추가

--- entity/Banner.java
+++ entity/Banner.java
@@ -39,6 +39,23 @@ public class Banner extends AuditingFields {
     @Column(nullable = false, length = 512)
     private String url;
 
+    // 배너가 표시될 위치 타입
+    @Enumerated(EnumType.STRING)
+    @Column(name = "location_type", nullable = false)
+    private BannerLocationType locationType = BannerLocationType.HOME;
+
+    // 구체적인 위치 식별자 (예: menuCode "101", "201" 또는 "air_search" 등)
+    @Column(name = "location_identifier", length = 50)
+    private String locationIdentifier;
+
+    public enum BannerLocationType {
+        HOME,           // 홈페이지 메인 배너
+        SUB_MENU,       // 서브메뉴 페이지 배너 (menuCode 사용)
+        AIR_SEARCH,     // 항공선택 페이지 배너
+        PRODUCT_LIST,   // 상품 목록 페이지 배너
+        PRODUCT_DETAIL  // 상품 상세 페이지 배너
+    }
+
     public Banner(Integer displayOrder, String title, LocalDate startDate, LocalDate endDate, String file, String url) {
         this.displayOrder = displayOrder;
         this.title = title;
@@ -47,6 +64,19 @@ public class Banner extends AuditingFields {
         this.file = file;
         this.url = url;
         this.active = true;
+        this.locationType = BannerLocationType.HOME;
+    }
+
+    public Banner(Integer displayOrder, String title, LocalDate startDate, LocalDate endDate, String file, String url, BannerLocationType locationType, String locationIdentifier) {
+        this.displayOrder = displayOrder;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.file = file;
+        this.url = url;
+        this.active = true;
+        this.locationType = locationType;
+        this.locationIdentifier = locationIdentifier;
     }
 }
 
